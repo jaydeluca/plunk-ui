93a6e14d4122be724ab46bbb17f4a82071973e92
Ian Arsenault <ian_arsenault@protonmail.com>
2018-09-21 21:47:12 -0400

    create store modules

--- client/src/main.js
+++ client/src/main.js
@@ -1,37 +1,10 @@
 import 'es6-promise/auto'
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import Vuex from 'vuex'
-import teamsService from './services/teams'
+import router from '.router'
+import store from './store/store'
 
 Vue.config.productionTip = false
-
-Vue.use(Vuex)
-
-const store = new Vuex.Store({
-  state: {
-    teams: [],
-    teamOne: {},
-    teamTwo: {},
-    playerA: {},
-    playerB: {},
-    playerC: {},
-    playerD: {},
-  },
-  actions: {
-    getAllTeams ({ commit }) {
-      teamsService.getTeams(teams => {
-        commit('setTeams', teams)
-      })
-    }
-  },
-  mutations: {
-    setTeams (state, teams) {
-      state.teams = teams
-    }
-  }
-})
 
 new Vue({
   router,

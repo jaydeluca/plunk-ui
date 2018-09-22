7705873b0f1d2f39ac4b6d3828651751e6e7fcd5
Ian Arsenault <ian_arsenault@protonmail.com>
2018-09-21 22:15:41 -0400

    update store index

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,8 @@
 import 'es6-promise/auto'
 import Vue from 'vue'
 import App from './App.vue'
-import router from '.router'
-import store from './store/store'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 

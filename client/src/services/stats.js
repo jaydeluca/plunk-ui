6dad5a1c1af8a8ee9fc20fe07a8c00aa68b1db7b
Jay DeLuca <jaydeluca4@gmail.com>
2018-11-10 21:52:18 -0500

    stats

--- client/src/services/stats.js
+++
@@ -1,9 +0,0 @@
-import client from '@/client'
-
-export default {
-  getStats (cb) {
-    const stats = client.get('/stats').then(res => {
-      return cb(res.data.content)
-    })
-  }
-}
--- client/src/views/Stats.vue
+++ client/src/views/Stats.vue
@@ -15,10 +15,39 @@
     </div>
 
     <div class="columns">
-      <div class="column is-half is-offset-one-quarter">
+      <div class="column is-fullwidth">
         <p class="title is-2">
           Stats
         </p>
+        <table class="table is-fullwidth is-striped is-bordered">
+          <thead>
+            <tr>
+              <th>Player</th>
+              <th>Team</th>
+              <th>Games Played</th>
+              <th>One Point</th>
+              <th>Plunks</th>
+              <th>Total Points</th>
+              <th>One Point / Game</th>
+              <th>Plunks / Game</th>
+              <th>Total Points / Game</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="stat in stats" :key="stat.player.id">
+              <td>{{ stat.player.name }}</td>
+              <td>{{ stat.team }}</td>
+              <td>{{ stat.gamesPlayed }}</td>
+              <td>{{ stat.points }}</td>
+              <td>{{ stat.plunks }}</td>
+              <td>{{ stat.totalPoints }}</td>
+              <td>{{ stat.onePointsPerGame }}</td>
+              <td>{{ stat.plunksPerGame }}</td>
+              <td>{{ stat.totalPointsPerGame }}</td>
+            </tr>
+          </tbody>
+        </table>
+
       </div>
     </div>
 
@@ -29,10 +58,10 @@
 
 <script>
 import { mapState } from 'vuex'
-import statsService from '@/services/stats'
+import client from '@/client'
 
 export default {
-  name: 'players',
+  name: 'stats',
   data: function() {
     return {
       stats: []
@@ -42,7 +71,9 @@ export default {
 
   },
   created () {
-    this.stats = statsService.getStats()
+    client.get('/stats').then(res => {
+      this.stats = res.data
+    })
   }
 }
 </script>
